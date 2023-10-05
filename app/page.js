738741d08b93c379a12e068a322db43f99d290a7
Bernard Uriza <bernard.uriza@planettogether.com>
2023-10-05 12:27:12 -0600

    New modification, on day on day 5 of October 2023 at 1:27:11 PM

--- app/page.js
+++ app/page.js
@@ -5,6 +5,7 @@ import Users from './Users';
 import Results from './Results';
 import { TabGroup, TabList, Tab, TabPanels, TabPanel } from "@tremor/react";
 import { HomeIcon, UserIcon, SearchIcon } from '@heroicons/react/outline';
+import { HiOutlineArrowRightOnRectangle } from "react-icons/hi2";
 
 const Page = () => {
   const [activeTab, setActiveTab] = useState('dashboard');
@@ -14,7 +15,25 @@ const Page = () => {
   };
 
   return (
-    <div>
+    <div className='container mx-auto my-5 font-sans'>
+
+      <div className="flex items-center my-5 space-x-4">
+        <div class="w-28">
+          <img src="/images/ferboliMovil.png" alt="Logo Image" className="mx-auto my-auto" />
+        </div>
+        <div class="flex-1">
+          <div class="text-gray-700 text-xl font-normal leading-7">Bienvenido, Dr. Ernesto Fernandez</div>
+          <div class="text-gray-500 text-base font-normal leading-5">Panel de administración</div>
+        </div>
+        <div className="w-28">
+          <button class="text-gray-500 hover:text-blue-700 flex items-center focus:outline-none">
+            Salir
+            <HiOutlineArrowRightOnRectangle className="mx-1 w-6 h-6" />
+          </button>
+        </div>
+      </div>
+
+
       <TabGroup>
         <TabList className="tabs">
           <Tab onClick={() => handleTabChange('dashboard')}>
