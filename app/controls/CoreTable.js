c38f433b8770ad63237aa246a924be5494226814
Bernard Uriza <bernard.uriza@planettogether.com>
2023-10-17 01:33:17 -0600

    New modification, on day on day 17 of October 2023 at 2:33:16 AM

--- app/controls/CoreTable.js
+++ app/controls/CoreTable.js
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Table, TableHead, TableHeaderCell, TableBody, TableRow, TableCell } from "@tremor/react";
 import { DownloadIcon, PrinterIcon, PencilIcon, SortAscendingIcon, SortDescendingIcon } from '@heroicons/react/outline';
 import TableCellButtonIcon from './TableCellButtonIcon';
-import CustomCheckbox from './CustomCheckbox'; 
+import CustomCheckbox from './CustomCheckbox';
 
 const CoreTable = ({
   data,
@@ -17,6 +17,7 @@ const CoreTable = ({
   const [selectAll, setSelectAll] = useState(false);
   const [sortedColumn, setSortedColumn] = useState(null);
   const [sortAscending, setSortAscending] = useState(true);
+  const [itemsSelected, setItemsSelected] = useState([]);
 
   const filteredData = data.filter((item) =>
     item.name.toLowerCase().includes(filterText.toLowerCase())
@@ -45,10 +46,6 @@ const CoreTable = ({
     pageNumber * itemsPerPage
   );
 
-  const handleSelectAll = () => {
-    setSelectAll(!selectAll);
-  };
-
   const handleSortColumn = (column) => {
     if (column === sortedColumn) {
       // Toggle sort order
@@ -60,12 +57,40 @@ const CoreTable = ({
     }
   };
 
+  const handleSelect = (item) => {
+    const selectedIndex = itemsSelected.indexOf(item);
+    const newSelected = [...itemsSelected];
+
+    if (selectedIndex === -1) {
+      newSelected.push(item);
+    } else {
+      newSelected.splice(selectedIndex, 1);
+    }
+
+    setItemsSelected(newSelected);
+  };
+
+  const handleSelectAll = () => {
+    if (!selectAll) {
+      // Select all items
+      setItemsSelected([...filteredDataWithColumnFilter]);
+    } else {
+      // Deselect all items
+      setItemsSelected([]);
+    }
+
+    setSelectAll(!selectAll);
+  };
+
   return (
     <Table>
       <TableHead className="bg-slate-50">
         <TableRow>
           <TableHeaderCell>
-            <CustomCheckbox checked={selectAll} onChange={handleSelectAll} />
+            <CustomCheckbox
+              checked={selectAll}
+              onChange={handleSelectAll}
+            />
           </TableHeaderCell>
           {columns.map((column) => (
             <TableHeaderCell
@@ -91,13 +116,16 @@ const CoreTable = ({
         {paginatedData.map((item) => (
           <TableRow key={item.id}>
             <TableCell>
-              <CustomCheckbox checked={selectAll} onChange={() => {}}/>
+              <CustomCheckbox
+                checked={itemsSelected.includes(item)}
+                onChange={() => handleSelect(item)}
+              />
             </TableCell>
             {columns.map((column) => (
               <TableCell key={column.key}>{renderCell(column.key, item)}</TableCell>
             ))}
             <TableCell>
-              <TableCellButtonIcon text={"Download"} icon={<DownloadIcon className="w-4 h-4" />} />
+              <TableCellButtonIcon text={"Downloa3d"} icon={<DownloadIcon className="w-4 h-4" />} />
               <TableCellButtonIcon text={"Print"} icon={<PrinterIcon className="w-4 h-4" />} />
               <TableCellButtonIcon onClick={() => openForm(item)} text={"Edit"} icon={<PencilIcon className="w-4 h-4" />} />
             </TableCell>
