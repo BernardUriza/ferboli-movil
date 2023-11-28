11327f15d0ccbf13416b1887d473904a0889eb30
Bernard Uriza Orozco <bernarduriza@gmail.com>
2023-11-28 15:56:58 -0600

    New modification, on day on day 28 of November 2023 at 3:56:58 PM

--- app/components/NumericIndicators.js
+++ app/components/NumericIndicators.js
@@ -1,14 +1,20 @@
 // NumericIndicators.js
 import React from 'react';
 import { Text, Metric, Card } from "@tremor/react";
+import { UserGroupIcon } from '@heroicons/react/outline';
 
 const NumericIndicators = () => {
   return (
     <div className="h-full">
       <div className="h-1/2 pb-3">
         <Card className="h-full">
-          <Text>Number of Clients</Text>
-          <Metric>42</Metric>
+          <div className='flex'>
+            <div>
+              <Text>Pacientes</Text>
+              <Metric>200</Metric>
+            </div>
+            <UserGroupIcon style={{width: "36px",height: "36px"}}></UserGroupIcon>
+          </div>
         </Card>
       </div>
       <div className="h-1/2">
