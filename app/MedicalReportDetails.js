8a5454f20d4c5341e89e99ecb4dd6983527e91e5
Bernard Uriza Orozco <bernarduriza@gmail.com>
2023-11-27 23:49:30 -0600

    New modification, on day on day 27 of November 2023 at 11:49:30 PM

--- app/MedicalReportDetails.js
+++ app/MedicalReportDetails.js
@@ -1,5 +1,7 @@
 import { useState, useEffect } from 'react';
 import { fetchMedicalReport } from './useCases/fetchMedicalReport'; 
+import { Frame } from './controls/Frame/frame';
+import { HiOutlineArrowRightOnRectangle } from "react-icons/hi2";
 
 const MedicalReportDetails = ({ medicalReportId }) => {
   const [patientData, setPatientData] = useState(null);
@@ -32,6 +34,16 @@ const MedicalReportDetails = ({ medicalReportId }) => {
 
   return (
     <div>
+      <link rel="stylesheet" href="/_next/static/css/app/layout.css?v=1701147868856" data-precedence="next_static/css/app/layout.css"></link>
+      <Frame></Frame>
+      <div className="flex mx-3">
+        <img src="/images/ferboliMovil.png" alt="Logo Image" className="mx-auto my-auto pt-3" width={105.426} height={40} />
+        <p className="flex-1"></p>
+        <a href='/api/auth/logout' className="text-gray-500 hover:text-blue-700 flex items-center focus:outline-none">
+          Salir
+          <HiOutlineArrowRightOnRectangle className="mx-1 w-6 h-6" />
+        </a>
+      </div>
       <h2>Patient Details</h2>
       <p>Name: {patientData.name}</p>
       <p>Email: {patientData.email}</p>
---
+++ app/controls/Frame/frame.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+export const Frame = () => {
+  return (
+    <div className="frame">
+      <p className="centro-de-diagn">
+        <span className="text-wrapper">Centro de </span>
+        <span className="text-wrapper font-bold">Diagnóstico Móvil</span>
+      </p>
+    </div>
+  );
+};
