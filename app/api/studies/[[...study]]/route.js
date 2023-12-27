372f57edd9df0944c1d5e1b2f427560bb98cdb0e
Bernard Uriza Orozco <bernarduriza@gmail.com>
2023-12-27 14:11:41 -0600

    New modification, on day on day 27 of December 2023 at 2:11:41 PM

--- app/api/studies/[[...study]]/route.js
+++ app/api/studies/[[...study]]/route.js
@@ -1,66 +1,67 @@
-// pages/api/studies/[[study]].js
-import { getAllStudies, createStudy, updateStudy, getStudyById } from '../../../../prisma/studiesClient';
+import { NextResponse } from 'next/server'; // Import NextResponse
+import {
+  getAllStudies,
+  createStudy,
+  updateStudy,
+  getStudyById
+} from '../../../../prisma/studiesClient';
 
-export default async (req, res) => {
+export async function GET(req, context) {
   const { method, query } = req;
   const { study } = query;
+  try {
+    if (study && study.length > 0) {
+      // If an ID is provided, fetch the specific study
+      const studyId = parseInt(study[0]);
+      const studyData = await getStudyById(studyId);
 
-  if (method === 'GET') {
-    try {
-      if (study && study.length > 0) {
-        // If an ID is provided, fetch the specific study
-        const studyId = parseInt(study[0]);
-        const studyData = await getStudyById(studyId);
-
-        if (studyData) {
-          return res.status(200).json(studyData);
-        } else {
-          return res.status(404).json({ error: 'Study not found' });
-        }
+      if (studyData) {
+        return NextResponse.json(studyData, { status: 200 });
       } else {
-        // If no ID is provided, fetch all studies
-        const studies = await getAllStudies();
-        return res.status(200).json(studies);
+        return NextResponse.json({ error: 'Study not found' }, { status: 404 });
       }
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: 'Error fetching studies' });
+    } else {
+      // If no ID is provided, fetch all studies
+      const studies = await getAllStudies();
+      return NextResponse.json(studies, { status: 200 });
     }
-  } else if (method === 'POST') {
-    try {
-      const { id, medicalReportId, name, type } = req.body;
-
-      // Validation of the fields
-      if (!medicalReportId || !type) {
-        return res.status(400).json({ error: 'All fields are required' });
-      }
-
-      // You may perform additional validations according to your needs, e.g., validate the date format or the value of the status.
-
-      // Check if the study already exists by its ID
-      const existingStudy = id == "" ? false : await getStudyById(id);
-
-      if (existingStudy) {
-        // If the study exists, update it
-        const updatedStudy = await updateStudy(id, {
-          name,
-          studyTypeId: type.id
-        });
-        res.status(200).json(updatedStudy);
-      } else {
-        // If the study does not exist, create it as a new study
-        const newStudy = await createStudy({
-          medicalReportId,
-          name,
-          studyTypeId: type.id, // categoryId instead of the complete category object
-        });
-        res.status(201).json(newStudy);
-      }
-    } catch (error) {
-      console.error(error);
-      return res.status(500).json({ error: 'Error saving study ' + error });
-    }
-  } else {
-    return res.status(405).json({ error: 'Method not allowed' });
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Error fetching studies' }, { status: 500 });
   }
-};
+}
+export async function POST(req, context) {
+  try {
+    const { id, medicalReportId, name, type } = req.body;
+
+    // Validation of the fields
+    if (!medicalReportId || !type) {
+      return NextResponse.json({ error: 'All fields are required' }, { status: 400 });
+    }
+
+    // You may perform additional validations according to your needs, e.g., validate the date format or the value of the status.
+
+    // Check if the study already exists by its ID
+    const existingStudy = id === "" ? false : await getStudyById(id);
+
+    if (existingStudy) {
+      // If the study exists, update it
+      const updatedStudy = await updateStudy(id, {
+        name,
+        studyTypeId: type.id
+      });
+      return NextResponse.json(updatedStudy, { status: 200 });
+    } else {
+      // If the study does not exist, create it as a new study
+      const newStudy = await createStudy({
+        medicalReportId,
+        name,
+        studyTypeId: type.id, // categoryId instead of the complete category object
+      });
+      return NextResponse.json(newStudy, { status: 201 });
+    }
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json({ error: 'Error saving study ' + error }, { status: 500 });
+  }
+}
