c42492fd32571d1a89d0375f0b19e5e4bf76dd0b
Bernard Uriza Orozco <bernarduriza@gmail.com>
2024-01-01 00:17:51 -0600

    New modification, on day on day 1 of January 2024 at 12:17:50 AM

--- app/controls/CustomModal/CustomModal.js
+++ app/controls/CustomModal/CustomModal.js
@@ -11,16 +11,17 @@ const CustomModal = ({
   modalClassName,
   footerElement
 }) => {
-  // State to manage background opacity
+  const [isClosing, setIsClosing] = useState(false);
   const [backgroundOpacity, setBackgroundOpacity] = useState(0);
 
   useEffect(() => {
     if (visible) {
-      // When modal becomes visible, fade in the background
-      setTimeout(() => setBackgroundOpacity(0.5), 100); // Adjust timing as needed
+      setIsClosing(false);
+      setBackgroundOpacity(0.5); // Fade in the background
     } else {
-      // When modal is not visible, remove the background
-      setBackgroundOpacity(0);
+      setIsClosing(true);
+      setBackgroundOpacity(0); // Start fade out
+      setTimeout(() => setIsClosing(false), 300); // Reset after animation
     }
   }, [visible]);
 
@@ -43,24 +44,29 @@ const CustomModal = ({
     '80': 'w-full sm:w-screen md:w-4/5 lg:w-3/4 xl:w-2/3'
   }[widthPercentage] || 'w-full sm:w-screen md:w-3/5 lg:w-2/3 xl:w-1/2';
 
-  const modalClasses = `fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-50 transition-opacity duration-300 ease-in-out ${visible ? 'opacity-100' : 'opacity-0 pointer-events-none'}`;
+  const modalClasses = `fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-50 ${visible || isClosing ? 'opacity-100' : 'opacity-0 pointer-events-none'} transition-opacity duration-300 ease-in-out`;
+
+  const handleCloseClick = () => {
+    setIsClosing(true); // Trigger close animations
+    setTimeout(onClose, 300); // Delay the actual closing to allow animations to play
+  };
 
   return (
     <div
       className={modalClasses}
-      style={{ backgroundColor: `rgba(0, 0, 0, ${backgroundOpacity})`, transition: 'background-color 0.5s ease' }} // Slow transition for background
-      onClick={onClose}
+      style={{ backgroundColor: `rgba(0, 0, 0, ${backgroundOpacity})`, transition: 'background-color 0.5s ease' }}
+      onClick={handleCloseClick}
     >
       <div
-        className={`flex flex-col p-4 mx-auto bg-white rounded-lg shadow-md dark:bg-gray-700 h-full md:h-auto ${modalWidth} ${modalClassName}`}
+        className={`flex flex-col p-4 mx-auto bg-white rounded-lg shadow-md dark:bg-gray-700 h-full md:h-auto ${modalWidth} ${modalClassName} ${isClosing ? 'animate-modalFadeOut' : 'animate-modalFadeIn'}`}
         onClick={(e) => e.stopPropagation()}
       >
         <div className="flex justify-between items-center">
           <h2 className={`text-xl font-medium text-gray-900 dark:text-white ${titleClassName}`}>{title}</h2>
           <button
-            onClick={onClose}
+            onClick={handleCloseClick}
             aria-label="Close"
-            className="text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg p-1 w-8 h-8 text-sm ml-auto dark:hover:bg-gray-600 dark:hover:text-white"
+            className="text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg p-1 w-8 h-8 text-sm ml-auto dark:hover:bg-gray-600 dark:hover:text-white shadow-effect"
           >
             <XCircleIcon className="w-6 h-6" />
           </button>
