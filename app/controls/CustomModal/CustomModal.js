bf02cbda9614096eaf8d929349475f95e9e0fe2b
Bernard Uriza Orozco <bernarduriza@gmail.com>
2024-01-02 20:54:20 -0600

    New modification, on day on day 2 of January 2024 at 8:54:20 PM

--- app/controls/CustomModal/CustomModal.js
+++ app/controls/CustomModal/CustomModal.js
@@ -1,5 +1,15 @@
 import { useEffect, useState } from 'react';
 import { XCircleIcon } from '@heroicons/react/outline';
+import { motion, AnimatePresence } from 'framer-motion';
+
+const animationVariants = {
+  fadeInOut: {
+    hidden: { opacity: 0, scale: 0.95 },
+    visible: { opacity: 1, scale: 1 },
+    exit: { opacity: 0, scale: 0.95 }
+  },
+  // Additional animation types can be defined here
+};
 
 const CustomModal = ({
   title,
@@ -11,80 +21,57 @@ const CustomModal = ({
   modalClassName,
   footerElement
 }) => {
-  const [isClosing, setIsClosing] = useState(false);
-  const [backgroundOpacity, setBackgroundOpacity] = useState(0);
 
+  const [showModal, setShowModal] = useState(true);
   useEffect(() => {
-    if (visible) {
-      setIsClosing(false);
-      setBackgroundOpacity(0.5); // Fade in the background
-    } else {
-      setIsClosing(true);
-      setBackgroundOpacity(0); // Start fade out
-      setTimeout(() => setIsClosing(false), 300); // Reset after animation
+    if (!showModal) {
+      setTimeout(onClose, 300); // Delay onClose to allow exit animation to complete
     }
-  }, [visible]);
-
-  useEffect(() => {
-    let timeoutId;
-  
-    if (visible) {
-      setIsClosing(false);
-      setBackgroundOpacity(0.5); // Fade in the background
-    } else {
-      setIsClosing(true); // Start fade out
-      setBackgroundOpacity(0);
-  
-      // Set a timeout before closing the modal completely
-      timeoutId = setTimeout(() => {
-        setIsClosing(false);
-        onClose(); // Call the onClose prop after the delay
-      }, 1000); // Delay of 1 second
-    }
-  
-    return () => {
-      clearTimeout(timeoutId); // Clear the timeout if the component unmounts
-    };
-  }, [visible, onClose]);  
+  }, [visible, onClose, showModal]);
 
   const modalWidth = {
     '20': 'w-full sm:w-screen md:w-2/5 lg:w-1/3 xl:w-1/4',
     '80': 'w-full sm:w-screen md:w-4/5 lg:w-3/4 xl:w-2/3'
   }[widthPercentage] || 'w-full sm:w-screen md:w-3/5 lg:w-2/3 xl:w-1/2';
 
-  const modalClasses = `fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-50 ${visible || isClosing ? 'opacity-100' : 'opacity-0 pointer-events-none'} transition-opacity duration-300 ease-in-out`;
-
-  const handleCloseClick = () => {
-    setIsClosing(true); // Trigger close animations
-    setTimeout(onClose, 1000); // Delay the actual closing to allow animations to play
-  };
-  // Determine the appropriate animation class
-  const animationClass = isClosing ? 'animate-fadeOut' : 'animate-fadeIn';
+  const animationType = process.env.NEXT_PUBLIC_MODAL_ANIMATION || 'fadeInOut';
 
   return (
-    <div
-      className={modalClasses}
-      style={{ backgroundColor: `rgba(0, 0, 0, ${backgroundOpacity})`, transition: 'background-color 0.5s ease' }}
-      onClick={handleCloseClick}
-    >
-       <div
-        className={`flex flex-col p-4 mx-auto bg-white rounded-lg shadow-md dark:bg-gray-700 h-full md:h-auto ${modalWidth} ${modalClassName} ${animationClass}`}
-        onClick={(e) => e.stopPropagation()}
-      >
-        <div className="flex justify-between items-center">
-          <h2 className={`text-xl font-medium text-gray-900 dark:text-white ${titleClassName}`}>{title}</h2>
-          <button
-            onClick={handleCloseClick}
-            aria-label="Close"
-            className="text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg p-1 w-8 h-8 text-sm ml-auto dark:hover:bg-gray-600 dark:hover:text-white shadow-effect"
-          >
-            <XCircleIcon className="w-6 h-6" />
-          </button>
+    <>
+      {visible && (
+        <div
+          className="fixed top-0 left-0 right-0 bottom-0 flex items-center justify-center z-50"
+          style={{ backgroundColor: 'rgba(0, 0, 0, 0.5)' }}
+        >
+          <AnimatePresence>
+            {showModal && (
+              <motion.div
+                className={`flex flex-col p-4 mx-auto bg-white rounded-lg shadow-md dark:bg-gray-700 h-full md:h-auto ${modalWidth} ${modalClassName}`}
+                onClick={(e) => e.stopPropagation()}
+                initial="hidden"
+                animate="visible"
+                exit="exit"
+                transition={{ duration: 0.3 }}
+                variants={animationVariants[animationType]}
+              >
+                <div className="flex justify-between items-center">
+                  <h2 className={`text-xl font-medium text-gray-900 dark:text-white ${titleClassName}`}>{title}</h2>
+                  <button
+                    onClick={() => setShowModal(false)} // Update this to match your state control logic
+                    aria-label="Close"
+                    className="text-gray-400 hover:bg-gray-200 hover:text-gray-900 rounded-lg p-1 w-8 h-8 text-sm ml-auto dark:hover:bg-gray-600 dark:hover:text-white shadow-effect"
+                  >
+                    <XCircleIcon className="w-6 h-6" />
+                  </button>
+                </div>
+                <div className="flex-grow mt-4">{children}</div>
+                <div className="mt-4">{footerElement}</div>
+              </motion.div>
+            )}
+          </AnimatePresence>
         </div>
-        <div className="flex-grow mt-4">{children}</div>
-        <div className="mt-4">{footerElement}</div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
