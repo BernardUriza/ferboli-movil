7d28b6bb2071d26898c676a5253a56ad91490604
Bernard Uriza Orozco <bernarduriza@gmail.com>
2024-01-04 13:57:59 -0600

    New modification, on day on day 4 of January 2024 at 1:57:59 PM

--- app/controls/StudieCard.js
+++ app/controls/StudieCard.js
@@ -30,7 +30,7 @@ const StudieCard = ({ clickFileLink, studieData, newCard, empty, openNewStudyFor
             </div>
             <div className='p-2'>
               <div className='font-bold'>Resultados de estudio</div>
-              <div>{empty}</div>              
+              <div>{empty}</div>
             </div>
           </a>
         </p>
@@ -38,7 +38,7 @@ const StudieCard = ({ clickFileLink, studieData, newCard, empty, openNewStudyFor
     );
   }
 
-  var {  type, createdAt } = studieData;
+  var { type, createdAt } = studieData;
   var { category, name } = type;
 
   // Formatea la fecha
@@ -47,22 +47,28 @@ const StudieCard = ({ clickFileLink, studieData, newCard, empty, openNewStudyFor
   return (
     <div className="bg-white rounded-lg p-4 shadow-md mb-3 mr-3" key={studieData.id} style={{ height: '110px' }}>
       <div className="flex justify-between items-center mb-2">
-        <div>
-          <div style={{ height: '40px', position: 'relative' }}>
-            <p className="text-gray-600 text-sm" style={{ position: 'absolute', bottom: 0 }}>{category.name}</p>
+        <div className='w-auto'>
+          <div style={{ height: '15px', position: 'relative' }}>
+            <p className="text-gray-600 text-sm overflow-hidden whitespace-nowrap truncate" style={{ position: 'absolute', bottom: 0 }} title={category.name}>{category.name}</p>
           </div>
-          <p className="text-lg font-bold">{name}</p>
+          <div style={{ height: '55px', position: 'relative' }}>
+            {/* Updated class and style for name */}
+            <p className="text-lg font-bold" title={name} style={{ maxHeight: '40px', maxWidth: '240px' }}>
+              {name}
+            </p>
+          </div>
           <p className="text-gray-600 text-sm line-clamp-1">Fecha: {formattedDate}</p>
         </div>
-          <a href={"/"} onClick={(e)=>{            
-              e.preventDefault();
-              clickFileLink(studieData);
-          }} rel="noopener noreferrer" className="text-green-500 line-clamp-1" style={{ fontSize: '13px' }}>
-            <div className="bg-green-100 items-center justify-center rounded-full p-2 mx-auto" style={{width: "35px"}}>
-              <DocumentTextIcon className="w-5 h-5" />
-            </div>
-            Ver documento
-          </a>
+
+        <a href={"/"} onClick={(e) => {
+          e.preventDefault();
+          clickFileLink(studieData);
+        }} rel="noopener noreferrer" className="text-green-500 line-clamp-1" style={{ fontSize: '13px', width: "100px !important" }}>
+          <div className="bg-green-100 items-center justify-center rounded-full p-2 mx-auto" style={{ width: "35px" }}>
+            <DocumentTextIcon className="w-5 h-5" />
+          </div>
+          <span>Ver documento</span>
+        </a>
       </div>
     </div>
   );
