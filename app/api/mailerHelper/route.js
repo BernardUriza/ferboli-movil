d9e9f59431fe0a30d2fb6b8fe9edd97adcc7a12e
Bernard Uriza Orozco <bernarduriza@gmail.com>
2024-01-30 16:35:13 -0600

    New modification, on day on day 30 of January 2024 at 4:35:12 PM

--- app/api/mailerHelper/route.js
+++ app/api/mailerHelper/route.js
@@ -1,55 +1,28 @@
 import { NextResponse } from 'next/server';
 import { mailOptions, transporter } from '../../config/nodemailer';
+const path = require('path');
+const fs = require('fs').promises;
 
-const generateEmailContent = (subject, text) => {
+const readHtmlFile = async (filePath) => {
+  try {
+      const content = await fs.readFile(filePath, 'utf8');
+      return content;
+  } catch (error) {
+      console.error('Error reading HTML file:', error);
+      throw error;
+  }
+};
+
+// Función para generar el contenido del correo
+const generateEmailContent = async (subject, text, htmlFilePath) => {
+  const htmlContent = await readHtmlFile(htmlFilePath);
   return {
-    text: 'stringData',
-    html: `
-      <!DOCTYPE html>
-      <html>
-        <head>
-          <meta charset="utf-8"/>
-          <meta name="viewport" content="width=device-width, initial-scale=1"/>
-          <meta http-equiv="X-UA-Compatible" content="IE=edge"/>
-          <style>
-            body {
-              font-family: Arial, sans-serif;
-              margin: 0;
-              padding: 0;
-              background: #ffffff;
-            }
-            .container {
-              max-width: 500px;
-              margin: 0 auto;
-              padding: 15px;
-              border: 1px dashed #ccc;
-            }
-            .header {
-              text-align: center;
-              font-size: 24px;
-              color: #2a2a2a;
-              margin-bottom: 20px;
-            }
-            .content {
-              font-size: 16px;
-              color: #2a2a2a;
-            }
-          </style>
-        </head>
-        <body>
-          <div class="container">
-            <div class="header">
-              ${subject}
-            </div>
-            <div class="content">
-              ${text}
-            </div>
-          </div>
-        </body>
-      </html>
-    `,
+      subject,
+      text,
+      html: htmlContent.replace("[Nombre del Usuario]","pamcho"),
   };
 };
+
 
 export async function POST(req) {
   const body = await req.json();
@@ -60,11 +33,14 @@ export async function POST(req) {
     return NextResponse.json({ message: 'Missing required fields ' + to }, { status: 400 });
   }
 
+  const htmlFilePath = path.join(__dirname, 'template.html');
+  const emailContent = await generateEmailContent(subject, text, htmlFilePath);
+
   mailOptions.to = to;
   try {
     await transporter.sendMail({
       ...mailOptions,
-      ...generateEmailContent(subject, text),
+      ...emailContent,
       subject: subject,
     });
 
