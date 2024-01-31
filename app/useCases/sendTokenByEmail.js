481c7b18442e3213a80ad0dc0e11c74a3296afbc
Bernard Uriza Orozco <bernarduriza@gmail.com>
2024-01-31 16:20:39 -0600

    New modification, on day on day 31 of January 2024 at 4:20:38 PM

--- app/useCases/sendTokenByEmail.js
+++ app/useCases/sendTokenByEmail.js
@@ -1,6 +1,6 @@
 const MedicalReport = require('../entities/MedicalReport');
 import { sendEmail } from '../lib/mailer';
-import { format } from 'date-fns'; 
+import { format } from 'date-fns';
 
 export default async function sendTokenByEmail(report) {
     try {
@@ -9,14 +9,20 @@ export default async function sendTokenByEmail(report) {
         const subject = 'Token para acceder a informes médicos';
         const to = medicalReport.patient.email;
         const nombreDeUsuario = medicalReport.patient.name;
-        const fecha = format(medicalReport.date, 'dd/MMMMM/yyyy'); // Format date
+        
+        let fecha = '';
+        if (medicalReport.date instanceof Date && !isNaN(medicalReport.date)) {
+            fecha = format(medicalReport.date, 'dd/MMMMM/yyyy');
+        } else {
+            // Handle invalid or undefined date here
+            console.error('Invalid or undefined date:', medicalReport.date);
+        }
         
         await sendEmail({ to, subject, url, nombreDeUsuario, fecha });
 
         return medicalReport;
     } catch (error) {
-        // Handle or log the error appropriately
         console.error('Error sending token by email:', error);
-        throw error; // or handle it as per your application's error handling policy
+        throw error;
     }
 };
