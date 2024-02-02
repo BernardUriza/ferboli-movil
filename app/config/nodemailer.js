3d7317b83c29a51e487f4b6735a890a8bc619bc4
Bernard Uriza Orozco <bernarduriza@gmail.com>
2024-02-01 20:23:22 -0600

    New modification, on day on day 1 of February 2024 at 8:23:22 PM

--- app/config/nodemailer.js
+++ app/config/nodemailer.js
@@ -1,13 +1,17 @@
 import nodemailer from "nodemailer";
 
+// Replace these with your actual email and password
 const email = process.env.EMAIL;
 const pass = process.env.EMAIL_PASS;
 
+
 export const transporter = nodemailer.createTransport({
-  service: "gmail",
+  host: "smtp.dreamhost.com", // SMTP server
+  port: 465, // SMTP Port
+  secure: true, // Use SSL/TLS
   auth: {
-    user: email,
-    pass,
+    user: email, // Your full email address
+    pass: pass, // Your email password
   },
 });
 
