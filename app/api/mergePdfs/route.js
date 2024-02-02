e15fcdff4ef05d3a96cd426548b9e14660da6441
Bernard Uriza Orozco <bernarduriza@gmail.com>
2024-02-02 13:17:53 -0600

    New modification, on day on day 2 of February 2024 at 1:17:52 PM

--- app/api/mergePdfs/route.js
+++ app/api/mergePdfs/route.js
@@ -1,28 +1,38 @@
-// pages/api/mergePdfs.js
+// /api/mergePdfs/route.js
 import { PDFDocument } from 'pdf-lib';
-import fetch from 'node-fetch';
+import { NextResponse } from 'next/server';
 
-export default async function handler(req, res) {
-  if (req.method === 'POST') {
-    try {
-      const { pdfUrls } = req.body; // Array de URLs de los PDFs a concatenar
-      const mergedPdf = await PDFDocument.create();
+// Manejador para el método POST
+export const POST = async (req) => {
+  try {
+    const { pdfUrls } = req.body; // Array de URLs de los PDFs a concatenar
+    const mergedPdf = await PDFDocument.create();
 
-      for (const pdfUrl of pdfUrls) {
-        const existingPdfBytes = await fetch(pdfUrl).then((res) => res.arrayBuffer());
-        const existingPdf = await PDFDocument.load(existingPdfBytes);
-        const copiedPages = await mergedPdf.copyPages(existingPdf, existingPdf.getPageIndices());
-        copiedPages.forEach((page) => mergedPdf.addPage(page));
-      }
-
-      const mergedPdfBytes = await mergedPdf.save();
-      res.setHeader('Content-Type', 'application/pdf');
-      res.send(Buffer.from(mergedPdfBytes));
-    } catch (error) {
-      res.status(500).json({ error: error.message });
+    for (const pdfUrl of pdfUrls) {
+      const existingPdfBytes = await fetch(pdfUrl).then((res) => res.arrayBuffer());
+      const existingPdf = await PDFDocument.load(existingPdfBytes);
+      const copiedPages = await mergedPdf.copyPages(existingPdf, existingPdf.getPageIndices());
+      copiedPages.forEach((page) => mergedPdf.addPage(page));
     }
-  } else {
-    res.setHeader('Allow', ['POST']);
-    res.status(405).end(`Method ${req.method} Not Allowed`);
+
+    const mergedPdfBytes = await mergedPdf.save();
+    
+    // Crear una instancia de NextResponse para manejar la respuesta
+    const response = new NextResponse(Buffer.from(mergedPdfBytes), {
+      headers: {
+        'Content-Type': 'application/pdf',
+      },
+    });
+
+    // No es necesario llamar a NextResponse aquí, ya que Response ya está configurado correctamente
+    return response;
+  } catch (error) {
+    // Usar NextResponse para manejar errores
+    return new NextResponse(JSON.stringify({ error: error.message }), {
+      status: 500,
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    });
   }
-}
+};
